be37418d2e0ae922b921744e8474b78f6614d92d
jvverde at win10 <jvverde@win10>
2020-11-05 00:08:26 +0000

    Dormir

--- examples.js
+++ examples.js
@@ -10,17 +10,63 @@ function getAllPropertyNames(obj) {
 }
 
 const today = new Date()
+const string = new String('abc')
+const bool = new Boolean()
+const number = new Number(3.14)
+const array = new Array(1,2)
 
-console.log(today)
-console.log(today.getTime())
-for (const i in today) {
-  console.log(`${i}:${today[i]}`)
+const text = 'texto'
+const falso = false
+const um = 1
+const vector = [1, 2]
+const mix = [1, {
+  a: 1,
+  b: 'dois',
+  today
+}]
+
+class A {
+  constructor(n) {
+    this.n = n
+  }
+  inc(n) {
+    this.n += n
+  }
+}
+const a = new A(10)
+
+const obj = {
+  today,
+  string,
+  text,
+  bool,
+  falso,
+  number,
+  um,
+  array,
+  vector,
+  mix,
+  f: function (a) {
+    return 5
+  },
+  g: (a) => a*a,
+  a
 }
 
-const s = new  String('abc')
-console.log(today instanceof Object)
-console.log(today instanceof Date)
-console.log(s instanceof Object)
-console.log(s instanceof String)
-// const json = serialize(today)
-// console.log(json)
+const pre = serialize(obj)
+
+console.log(pre)
+console.log(JSON.stringify(pre))
+
+const oo = deserialize(pre)
+console.log(oo)
+
+
+/*const test = Object.create(Array.prototype)
+test[3] = 'c'
+console.log(test)
+console.log(Array.from({ '0': 1, '1': 2 }))
+console.log(Array.from(test))
+console.log({
+  a: test
+})*/
--- index.js
+++ index.js
@@ -1,18 +1,43 @@
 const isValidVarName = require('./isValidVarName')
 
-const replacer = (key, value) => {
-  if (value && value instanceof Object && value.constructor
-    && value.constructor.name && value.constructor !== Object
-    && value.constructor !== String && value.constructor !== Number
-    && value.constructor !== Symbol && value.constructor !== Boolean
-    && value.constructor !== Array && value.constructor !== Function
-    ) {
-    return {
-      _class: value.constructor.name,
-      _key: key,
-      _value: replacer(Object.assign({}, value))
+const modify = (obj) => {
+  if (obj && obj instanceof Object) {
+    // console.log('is obj:', obj)
+    const keys = Object.keys(obj)
+    if (keys.length) {
+      const newobj = {}
+      keys.forEach(k => {
+        newobj[k] = replacer(obj[k])
+      })
+      return newobj
+    } else {
+      console.log('No keys for', obj)
+      return replacer(obj)
     }
   }
+  return obj
+}
+const replacer = (value) => {
+  // console.log('k,v =', key, value)
+  if (value && value instanceof Object && value.constructor
+    && value.constructor.name // && value.constructor !== Object
+    // && value.constructor !== String && value.constructor !== Number
+    // && value.constructor !== Symbol && value.constructor !== Boolean
+    //  && value.constructor !== Array && value.constructor !== Function
+    ) {
+    const _class = value.constructor.name
+    let _value
+    if (value instanceof Array) {
+      _value = value.map(v => modify(v))
+    } else {
+      _value = ktypes.includes(_class) ? value : modify(Object.assign({}, value))
+    }
+    return {
+      _class,
+      _value
+    }
+  }
+ //  console.log("Didn't replace", value)
   return value
 }
 const reviver = (...classes) => {
@@ -29,5 +54,44 @@ const reviver = (...classes) => {
     return value
   }
 }
-module.exports.serialize = (object)  => replacer(object)
-module.exports.deserialize = (string, ...classes) => JSON.parse(string, reviver(...classes))
+const types = {
+  Date: v => new Date(v),
+  Number: v => new Number(v),
+  String: v => new String(v),
+  Boolean: v => new Boolean(v),
+  Function: v => new Function(v)
+}
+const ktypes = Object.keys(types)
+
+const reconstruct = (obj, ...classes) => {
+  const lookup = {}
+  classes.forEach(c => { lookup[c.name] = c.prototype })
+
+  function f (obj) {
+    console.log('obj', typeof obj)
+    if(obj && obj instanceof Object && obj._class && typeof obj._class === 'string' && obj._value !== undefined) {
+      if (obj._class === 'Array') {
+        return Object.keys(obj._values).map(k => f(obj._values[k]))
+      } else if (lookup[obj._class]) {
+        const prototype = lookup[obj._class]
+        const children = f(obj._value)
+        const descriptors = Object.getOwnPropertyDescriptors(children)
+        return Object.create(prototype, descriptors)
+      } else if (ktypes.includes(obj._class)) {
+        return types[obj._class](obj._value)
+      }
+    } else if(obj && obj instanceof Object) {
+      const newobj = new Object()
+      for (i in obj) {
+        newobj[i] = f(obj[i])
+      }
+      return newobj
+    }
+    return obj
+  }
+  return f(obj)
+}
+
+module.exports.serialize = (object)  => modify(object)
+// module.exports.deserialize = (string, ...classes) => JSON.parse(string, reviver(...classes))
+module.exports.deserialize = (obj, ...classes) => reconstruct(obj, ...classes)
