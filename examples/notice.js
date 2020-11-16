22a028d8e1b2078084030dbd204fab8df45bb725
jvverde at win10 <jvverde@win10>
2020-11-16 20:32:34 +0000

    better approach

--- examples/notice.js
+++ examples/notice.js
@@ -12,34 +12,34 @@ function serdes(obj, debug = false, strictly = true){
   return newobj
 }
 
-// serdes()
-// serdes(undefined)
-// serdes(null)
-// serdes(1)
-// serdes('a')
-// serdes({})
-// serdes([])
-// serdes({i:1})
-// serdes([1])
-// serdes({j:[]})
-// serdes([{}])
-// serdes({k:[1]})
-// serdes([{k:1}])
-// serdes(new Date)
-// serdes(/^$/igsm)
-// serdes(3n)
-// serdes(new Set)
-// serdes(new Map)
-// serdes(new Set([3,2,1]))
-// serdes(new Set([{i:2},{i:2}]))
+serdes()
+serdes(undefined)
+serdes(null)
+serdes(1)
+serdes('a')
+serdes({})
+serdes([])
+serdes({i:1})
+serdes([1])
+serdes({j:[]})
+serdes([{}])
+serdes({k:[1]})
+serdes([{k:1}])
+serdes(new Date)
+serdes(/^$/igsm)
+serdes(3n)
+serdes(new Set)
+serdes(new Map)
+serdes(new Set([3,2,1]))
+serdes(new Set([{i:2},{i:2}]))
 class A{
   constructor(n) {
     this.n = n
   }
   get val() { return this.n }
 }
-//serdes(new A(3), false, false)
-//serdes(new Set([new A(3)]), false, false)
+serdes(new A(3), false, false)
+serdes(new Set([new A(3)]), false, false)
 
 class Int extends Number{
   // static cnt = 0
@@ -54,6 +54,9 @@ class Int extends Number{
   //  this.i +  v
   //}
 }
+
+serdes(new Int(3), false, false)
+
 class Long extends Int{
   constructor(i = 5) {
     super(i)
@@ -62,40 +65,62 @@ class Long extends Int{
   get long() { return this}
 }
 
-// serdes(new Int(3), false, false)
-// serdes(new Long(3), false, false)
+serdes(new Long(3), false, false)
 
 class Sset extends Set{
   constructor(s){
     super(s)
     this.label = 'this is a set'
-
   }
 }
-//serdes(new Set(), false, false)
-serdes(new Sset([{i:1}]), false, false)
+// serdes(new Set(), false, false)
+// serdes(new Sset([{i:1}]), false, false)
 class Smap extends Map{
   constructor(s) {
     super(s)
   }
   get name() {return 'my name'}
 }
-serdes(new Smap([[{i:1}, {j:3}]]), false, false)
+// serdes(new Smap([[{i:1}, {j:3}]]), false, false)
 //console.log(Object.getOwnPropertyDescriptors(new Sset()))
+
+/*function types(a, type = Object) {
+  console.log('-------', a)
+  try {
+    console.log('typeof a:', typeof a)
+    console.log(`a instanceof ${type.name}:`, a instanceof type)
+    console.log('a instanceof Object:', a instanceof Object)
+    console.log('a.constructor:', a.constructor)
+    console.log('a.constructor.name:', a.constructor.name)
+  } catch (e) {}
+}
+types('a', String)
+types(new String('a'), String)
+types(0, Number)
+types(new Number(0), Number)
+types(true, Boolean)
+types(new Boolean(true), Boolean)
+types(/^$/i, RegExp)
+types(new RegExp('^$','i'), RegExp)
+types(1n, BigInt)
+types(BigInt(1), BigInt)
+types(null, Object)
+types(NaN, Object)
+types(undefined, Object)
+types(Infinity, Object)*/
 
 const isIterable = obj => obj && typeof obj[Symbol.iterator] === 'function'
 
-function showchain(obj) {
+function showchain(obj, isPrototype = false) {
   let sp=''
-  const o = obj
   do {
     console.log(sp, 'obj:', obj)
     try {
       if (obj.valueOf instanceof Function) {
-        console.log(sp, 'valueof=', obj.valueOf())
+        console.log(sp, 'valueof:', obj.valueOf())
       }
       if (obj.toString instanceof Function) {
-        console.log(sp, 'toString=', obj.toString())
+        console.log(sp, 'toString:', obj.toString())
       }
     } catch (e) {
       console.warn(e)
@@ -103,9 +128,8 @@ function showchain(obj) {
     console.log(sp, 'typeof:', typeof obj)
     console.log(sp, 'constructor:', obj.constructor)
     console.log(sp, 'constructor.name:', obj.constructor.name)
-    console.log(sp, 'descriptors', Object.getOwnPropertyDescriptors(obj))
     console.log(sp, 'isIterable', isIterable(obj))
-    if (isIterable(obj)) {
+    if (!isPrototype && isIterable(obj)) {
       try {
         [...obj].forEach(e => {
           console.log(sp, 'e:', e)
@@ -114,8 +138,10 @@ function showchain(obj) {
         console.warn(e)
       }
     }
+    console.log(sp, 'descriptors', Object.getOwnPropertyDescriptors(obj))
     console.log(sp, '<<<<<<<<')
     sp += '  '
+    isPrototype = true
   } while(obj = Object.getPrototypeOf(obj))
   console.log('>>>>>>>>')
 }
--- index.js
+++ index.js
@@ -1,73 +1,80 @@
 const isClone = require('isClone')
-const basicTypes = [Number, String, Boolean]
-const builtinTypes = { // javascript builtin objects
-  Number: v => new Number(v),
-  String: v => new String(v),
-  Boolean: v => new Boolean(v),
-  Date: v => new Date(v),
-  BigInt: v => BigInt(v),
-  RegExp: v => {
-    const exp = new RegExp(v.source, v.flags)
-    if (v.lastIndex) exp.lastIndex = v.lastIndex
-    return exp
-  },
-  Symbol: v => Symbol(v),
-  Error: v => {
-    const err = new Error(v.message || '')
-    if (v.name) err.name = v.name
-    return err
-  },
-  Set: (v, compose) => new Set(v.map(e => compose(e))),
-  Map: (v, compose) => new Map(v.map(e => compose(e))),
-  Int8Array: v => new Int8Array(v),
-  Uint8Array: v => new Uint8Array(v),
-  Int16Array: v => new Int16Array(v),
-  Uint16Array: v => new Uint16Array(v),
-  Int32Array: v => new Int32Array(v),
-  Uint32Array: v => new Uint32Array(v),
-  Float32Array: v => new Float32Array(v),
-  Float64Array: v => new Float64Array(v),
-  Uint8ClampedArray: v => new Uint8ClampedArray(v),
-  BigUint64Array: (v, compose) => new BigUint64Array(v.map(e => compose(e))),
-  BigInt64Array: (v, compose) => new BigInt64Array(v.map(e => compose(e))),
-  // Idea from https://ovaraksin.blogspot.com/2013/10/pass-javascript-function-via-json.html
-  Function: v => new Function('return ' + v)()
-}
-
-const weirdTypes = {
-  Function: v => {
-    const string = v.toString()
-    if (string.indexOf('[native code]') > -1) {
-      return v.name
-    }
-    return string
-  },
-  BigInt: v => v.toString(),
-  Symbol: v => v.description,
-  Error: v => {
-    const { message, name } = v
-    return { message, name }
-  },
-  RegExp: v => {
-    const { source, flags, lastIndex } = v
-    return { source, flags, lastIndex }
-  },
-  Set: v => [...v].map(v => decompose(v)),
-  Map: v => [...v].map(v => decompose(v)),
-  Int8Array: v => Array.from(v),
-  Uint8Array: v => Array.from(v),
-  Int16Array: v => Array.from(v),
-  Uint16Array: v => Array.from(v),
-  Int32Array: v => Array.from(v),
-  Uint32Array: v => Array.from(v),
-  Float32Array: v => Array.from(v),
-  Float64Array: v => Array.from(v),
-  Uint8ClampedArray: v => Array.from(v),
-  BigUint64Array: v => Array.from(v).map(e => decompose(e)),
-  BigInt64Array: v => Array.from(v).map(e => decompose(e))
-}
 
 const isIterable = obj => obj && typeof obj[Symbol.iterator] === 'function'
+const basicTypes = [Number, String, Boolean]
+const transform = (assembly, dismantle = v => v) => {
+  return {
+    dismantle,
+    assembly
+  }
+}
+const builtins = { // javascript builtin objects
+  Number: transform(v => new Number(v)),
+  String: transform(v => new String(v)),
+  Boolean: transform(v => new Boolean(v)),
+  Date: transform(v => new Date(v)),
+  BigInt: transform(v => BigInt(v), v => v.toString()),
+  RegExp: transform(
+    v => {
+      const exp = new RegExp(v.source, v.flags)
+      if (v.lastIndex) exp.lastIndex = v.lastIndex
+      return exp
+    },
+    v => {
+      const { source, flags, lastIndex } = v
+      return { source, flags, lastIndex }
+    }
+  ),
+  Symbol: transform(v => Symbol(v), v => v.description),
+  Error: transform(
+    v => {
+      const err = new Error(v.message || '')
+      if (v.name) err.name = v.name
+      return err
+    },
+    v => {
+      const { message, name } = v
+      return { message, name }
+    }
+  ),
+  Set: transform(
+    (v, compose) => new Set(v.map(e => compose(e))),
+    (v, decompose) => [...v].map(v => decompose(v))
+  ),
+  Map: transform(
+    (v, compose) => new Map(v.map(e => compose(e))),
+    (v, decompose) => [...v].map(v => decompose(v))
+  ),
+  Int8Array: transform(v => new Int8Array(v), v => Array.from(v)),
+  Uint8Array: transform(v => new Uint8Array(v), v => Array.from(v)),
+  Int16Array: transform(v => new Int16Array(v), v => Array.from(v)),
+  Uint16Array: transform(v => new Uint16Array(v), v => Array.from(v)),
+  Int32Array: transform(v => new Int32Array(v), v => Array.from(v)),
+  Uint32Array: transform(v => new Uint32Array(v), v => Array.from(v)),
+  Float32Array: transform(v => new Float32Array(v), v => Array.from(v)),
+  Float64Array: transform(v => new Float64Array(v), v => Array.from(v)),
+  Uint8ClampedArray: transform(v => new Uint8ClampedArray(v), v => Array.from(v)),
+  BigUint64Array: transform(
+    (v, compose) => new BigUint64Array(v.map(e => compose(e))),
+    (v, decompose) => Array.from(v).map(e => decompose(e))
+  ),
+  BigInt64Array: transform(
+    (v, compose) => new BigInt64Array(v.map(e => compose(e))),
+    (v, decompose) => Array.from(v).map(e => decompose(e))
+  ),
+  Function: transform(
+    // Idea from https://ovaraksin.blogspot.com/2013/10/pass-javascript-function-via-json.html
+    v => new Function('return ' + v)(),
+    v => {
+      const string = v.toString()
+      if (string.indexOf('[native code]') > -1) {
+        return v.name
+      }
+      return string
+    }
+  )
+}
+
 
 // Get descriptors of Object prototype
 const protodescriptors = Object.getOwnPropertyDescriptors(Object.getPrototypeOf({}))
@@ -93,30 +100,30 @@ function isInstanceOf(obj, type) { // Check if object is instance of a given typ
   }
   return false
 }
-const bTypesNames = Object.keys(builtinTypes) // Avoid to get it everytime
-function getValue(obj) {
+const bTypesNames = Object.keys(builtins) // Avoid to get it everytime
+function getTypeValue(obj) {
   try { // Avoid TypeError: Number.prototype.valueOf requires that 'this' be a Number
     for(const typename of bTypesNames) {
-      if (isInstanceOf(obj, typename)) return builtinTypes[typename](obj)
+      if (isInstanceOf(obj, typename)) return {
+        type: typename,
+        value: builtins[typename].dismantle(obj)
+      }
     }
   } catch (e) {}
-  return undefined
+  return {}
 }
 const decompose = (obj, isPrototype = false) => {
   //console.log('obj:', obj)
-  if (obj === undefined) { return { _class: 'undefined' } }
-  //console.log('obj.constructor:', obj.constructor)
-  //console.log('obj.constructor.name:', (obj.constructor || {}).name)
-  //console.log('obj instanceof Object:', obj instanceof Object)
-  // if (obj && obj instanceof Object && obj.constructor && obj.constructor.name ) {
-  // Change from above test as it reveals the BigInt is not an Object!!!
-  if (obj && obj.constructor && obj.constructor.name
-    // don't decompose if obj is a basic type, unless it is defined as an object (ex: let i = new Number())
-    && (!basicTypes.includes(obj.constructor) || obj instanceof Object)){
+  if (arguments.length === 0) { return obj }
+  else if (obj === undefined) { return { _class: 'undefined' } }
+  else if (obj === null) { return obj }
+  else if (!obj.constructor || !obj.constructor.name) { return obj }
+  else if (typeof obj !== 'object' && basicTypes.includes(obj.constructor)) { return obj }
+  else {
     const _class = obj.constructor.name
     // console.log('class', _class)
-    if (weirdTypes[_class]) {
-      //console.log('weirdTypes')
+    if (builtins[_class]) {
+      //console.log('dismantle')
       if (isPrototype && isIterable(obj)) {
         //console.log('isPrototype')
         return {
@@ -126,13 +133,7 @@ const decompose = (obj, isPrototype = false) => {
       }
       return {
         _class,
-        _value: weirdTypes[_class](obj)
-      }
-    } else if (builtinTypes[_class]) {
-      //console.log('builtinTypes')
-      return {
-        _class,
-        _value: obj
+        _value: builtins[_class].dismantle(obj, decompose)
       }
     } else if(obj.constructor === Array || obj.constructor === Object) {
       //console.log('Array or Object')
@@ -141,7 +142,6 @@ const decompose = (obj, isPrototype = false) => {
         _descriptors: decompDescriptors(Object.getOwnPropertyDescriptors(obj))
       }
     } else {
-      //console.log('None of above')
       let _value, _parent
       if (!isPrototype) { // Don't do this for prototypes
         if (obj instanceof Set) {
@@ -151,7 +151,9 @@ const decompose = (obj, isPrototype = false) => {
           _parent = 'Map'
           _value = [...obj].map(v => decompose(v))
         } else {
-          _value = getValue(obj) // For cases where obj is an instance of sub class of a builtin type
+          const { value, type } = getTypeValue(obj) // For cases where obj is an instance of sub class of a builtin type
+          _value = value
+          _parent = type
         }
       }
       const _descriptors = decompDescriptors(Object.getOwnPropertyDescriptors(obj))
@@ -165,9 +167,6 @@ const decompose = (obj, isPrototype = false) => {
       }
     }
   }
-  // Don't need to decompose
-  // console.log('Do nothing for:', obj)
-  return obj
 }
 
 const reconstruct = (obj, ...classes) => {
@@ -212,25 +211,19 @@ const reconstruct = (obj, ...classes) => {
             const prototype = Object.getPrototypeOf(parent)
             const descriptors = compdesc(obj._descriptors)
             const newobj = Object.create(prototype, descriptors)
-            if (obj._value !== undefined) {
-              if (obj._parent && builtinTypes[obj._parent]) {
-                // Special cases where object is an instance of a sub class of a builtin object
-                const parent = builtinTypes[obj._parent](obj._value, compose)
-                const base = new newobj.constructor(parent)
-                return Object.assign(base, newobj)
-              } else {
-                // Another special cases where object is an instance of a sub class of simple builtin object
-                const base = new newobj.constructor(obj._value)
-                return Object.assign(base, newobj)
-              }
+            if (obj._parent && builtins[obj._parent]) {
+              // Special cases where object is an instance of a sub class of a builtin object
+              const parent = builtins[obj._parent].assembly(obj._value, compose)
+              const base = new newobj.constructor(parent)
+              return Object.assign(base, newobj)
             }
             return newobj
           } else {
             console.log("SHOULDN't HAPPEN")
           }
-        } else if (obj._value !== undefined && builtinTypes[obj._class]) {
+        } else if (obj._value !== undefined && builtins[obj._class]) {
           // for builtin objects
-          const value = builtinTypes[obj._class](obj._value, compose)
+          const value = builtins[obj._class].assembly(obj._value, compose)
           if (obj._class === 'Function' && value.name) {
             // temporary publish on global scope the named functions and classes
             const f = value
