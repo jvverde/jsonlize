3511ad6b1383e35bc33e463cec1abdef6b9b2d48
jvverde at win10 <jvverde@win10>
2020-11-27 01:25:57 +0000

    better code

--- examples/09-extendsBuiltin.js
+++ examples/09-extendsBuiltin.js
@@ -45,6 +45,12 @@ class Long extends Int {
   assert(m.count === 2)
 })()
 
+//const n = new Int(3)
+//const m = new Number(6)
+//console.log(Object.assign(m, n))
+//console.log(m.label)
+
+
 /*
 class Sset extends Set {
   constructor (s) {
