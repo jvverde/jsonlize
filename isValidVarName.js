d45e2a15882e54d81dcf4e717d050389675a830f
jvverde at win10 <jvverde@win10>
2020-12-02 12:35:11 +0000

    remove isValidVarName.js

--- isValidVarName.js
+++
@@ -1,9 +0,0 @@
-module.exports = name => {
-  // Get idea from https://stackoverflow.com/a/19265747
-  try {
-    new Function(`let ${name} = 0; ${name}++`)
-  } catch (e) {
-    return false
-  }
-  return true
-}
